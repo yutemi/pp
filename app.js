869fa3b68b64dc58a0d0c0dd6702d1eb97c442b3
yutemi <sayshade@yandex.ru>
2023-12-10 16:23:46 +0500

    add routes

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ const mongoose = require("mongoose")
 
 const app = express()
 
+app.use("/api/auth")
+
 const PORT = config.get("port") || 5000
 
 async function start() {
     try {
-        await mongoose.connect()
+        await mongoose.connect(config.get("mongoUri", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        }))
+        app.listen(5000, () => console.log(`123, port: ${PORT}`))
     } catch (error) {
         console.log(error.message)
         process.exit()
@@ -16,4 +23,3 @@ async function start() {
 }
 
 start()
-app.listen(5000, () => console.log(`123, port: ${PORT}`))
