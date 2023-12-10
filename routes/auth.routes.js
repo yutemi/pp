56ed96ee203d9b7c4a99bc3abc46c6230589dc32
yutemi <sayshade@yandex.ru>
2023-12-10 16:32:09 +0500

    add user.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,4 +1,17 @@
 const {Router} = require("express")
+const User = require("../models/User")
 const router = Router()
+
+router.post("/register", async(req, res) => {
+    try {
+        const {email, pass} = req.body
+    } catch (error) {
+        res.status(500).json({message: "pizda"})
+    }
+})
+
+router.post("/login", async(req, res) => {
+
+})
 
 module.exports = router
